dad7929d85e171d7efe495636b4de118c1813e6a
Sam Garson <samtgarson@gmail.com>
2019-03-30 19:50:32 +0000

    Setup and vue-anime

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueAnime from 'vue-animejs'
+import App from './App'
+
+Vue.use(VueAnime)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
